4206a459cd939e0d14228151983fc700ce993470
Huỳnh Phương Khanh <hpkhanh1610@gmail.com>
2023-08-28 11:31:18 +0700

    Added my project

---
+++ e2e/starter.e2e.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+describe('Example', () => {
+  // beforeAll(async () => {
+  //   await device.launchApp();
+  // });
+
+  beforeEach(async () => {
+    await device.launchApp({newInstance: true});
+  });
+
+  it('should enable swiping back and forth', async () => {
+    // TODO: Complete the test
+  });
+
+  it('should render "Debug" and have a Button to click in the third slide', async () => {
+    // TODO: Complete the test
+  });
+
+  it('should render "Learn More" and change text in the fourth slide', async () => {
+    // TODO: Complete the test
+  });
+});
